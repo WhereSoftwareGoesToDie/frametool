7be87bc5f7c163d68812d5ce28506175bac2c12c
Sharif Olorin <sio@tesser.org>
2014-03-25 17:06:48 +1100

    Need a split mode too

--- frametool.go
+++ frametool.go
@@ -39,6 +39,10 @@ type Config struct {
 	Gen struct {
 		Count uint
 	}
+	Split struct {
+		ChunkSize uint64
+		ChunkPrefix string
+	}
 }
 
 func main() {
@@ -48,10 +52,14 @@ func main() {
 	cmdHelp["cat"] = "Write frames to stdout."
 	cmds["gen"] = GenCommand
 	cmdHelp["gen"] = "Generate random frames for testing."
+	cmds["split"] = SplitCommand
+	cmdHelp["split"] = "Split a burst into multiple smaller bursts."
 
 	outputFormat := flag.String("output-fmt", "raw", "Encoding to use for writing frames. One of (raw,json).")
 	burstPacking := flag.Bool("output-burst", false, "Pack output as DataBursts instead of DataFrames.")
 	outputDest := flag.String("output", "", "File to output to. If not specified, output is written to stdout.")
+	maxChunkSize := flag.Uint64("max-chunk-size", 1024, "Maximum size of bursts to output when splitting.")
+	chunkPrefix := flag.String("chunk-prefix", "databurst", "Filename prefix to use when splitting bursts.")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [options] <cmd>\n\n", os.Args[0])
@@ -74,6 +82,8 @@ func main() {
 	} else {
 		cfg.Output.File = *outputDest
 	}
+	cfg.Split.ChunkSize = *maxChunkSize
+	cfg.Split.ChunkPrefix = *chunkPrefix
 	if flag.NArg() < 1 {
 		flag.Usage()
 		os.Exit(1)
---
+++ split.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func SplitCommand(cfg *Config, r FrameReader, w Writer) int {
+	fmt.Fprintf(os.Stderr, "Not yet implemented.\n")
+	return 0
+}
